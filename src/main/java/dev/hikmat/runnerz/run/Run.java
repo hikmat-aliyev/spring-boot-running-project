783118aaba5c1139c33c6cd3f29052daaef3c24b
Hikmat Ali <hikmat.aliyevm@gmail.com>
2024-10-04 16:10:20 +0400

    Add validation dependecy and use them in Run model

--- src/main/java/dev/hikmat/runnerz/run/Run.java
+++ src/main/java/dev/hikmat/runnerz/run/Run.java
@@ -2,12 +2,15 @@ package dev.hikmat.runnerz.run;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 public record Run(
-    Integer id,
-    String title,
-    LocalDateTime startedOn,
-    LocalDateTime completedOn,
-    Integer miles,
-    Location location) {
+        Integer id,
+        @NotEmpty String title,
+        LocalDateTime startedOn,
+        LocalDateTime completedOn,
+        @Positive Integer miles,
+        Location location) {
 
 }
