909510a92ab16af8cca41f42a9c5d78a9f105edc
Hikmat Ali <hikmat.aliyevm@gmail.com>
2024-10-07 16:10:04 +0400

    Add runs.json file

--- src/main/java/dev/hikmat/runnerz/Application.java
+++ src/main/java/dev/hikmat/runnerz/Application.java
@@ -1,13 +1,32 @@
 package dev.hikmat.runnerz;
 
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dev.hikmat.runnerz.run.Location;
+import dev.hikmat.runnerz.run.Run;
+import dev.hikmat.runnerz.run.RunRepository;
 
 @SpringBootApplication
 public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	// @Bean
+	// CommandLineRunner runner(RunRepository runRepository) {
+	// return args -> {
+	// Run run = new Run(1, "Morning Jog", LocalDateTime.now(), LocalDateTime.now(),
+	// 3, Location.OUTDOOR);
+	// runRepository.create(run);
+	// };
+	// }
 }
